de8cf190690b0c30e2f864cbcae16a61b10a172e
stankajofre <157607276+stankajofre@users.noreply.github.com>
2024-04-24 13:48:37 -0400

    cambios

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
-import 'dotenv/config'
 import express, { urlencoded } from 'express';
 import Jimp from 'jimp';
-import { v4 as uuidv4 } from 'uuid';
 import { nanoid } from 'nanoid'
 
-const_dirname = import.meta.dirname;
+const _dirname = import.meta.dirname;
+const app = express()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.static(_dirname + '/public'))
 
+app.get('/image', async (req, res) => {
 
-app.use(urlencoded({ extended: true }));
+    req.query
+    const image = await Jimp.read(`https://picsum.photos/1000`)
 
-app.get('/', (req, res) => {
-    console.log("estoy en la ruta principal")
-});
+    const image = await Jimp.read(image_url)
+    const buffer = await image
+        .resize(500, 500)
+        .grayscale()
+        .quality(60)
+        .getBufferAsync(Jimp.MIME_JPEG)
 
-app.post('/image', async (req, res) => {
-    const image_url = req.query.image_url
+    const dirname = __dirname + `/public/img/image-${nanoid()}.jpeg`
+    await image.writeAsync(dirname)
 
-    app.get('/image', async (req, res) => {
-        const image = await Jimp.read(image_url)
-        const buffer = await image
+    res.set("Content-Type", "image/jpeg")
+    return res.send(buffer)
 
-            .resize(350, AUTO) // Redimensionar a 350px de ancho
-            .grayscale() // Convertir a escala de grises
-            .quality(100) // Calidad de la imagen
-            .write(`public/images/${uuidv4()}.jpg`); // Guardar la imagen procesada con un nombre único
-            .getBufferAsync(Jimp.MIME_JPEG)
-
-        const dirname = __dirname + `/public/img/image-${nanoid()}.jpeg`
-        await image.writeAsync(dirname)
-
-        res.set("Content-Type", "image/jpeg")
-        return res.send(buffer)
-    })
+})
 
 
-    app.listen(PORT, () => {
-        console.log(`Servidor iniciado en http://localhost:${PORT}`);
-    })
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Example app listening on PORT ${PORT}`)
+    console.log(process.pid)
+
+})
